a5cd25c3fe659b9f999dd876e766956d1821ac1d
undernotic <undernotic@gmail.com>
2020-01-10 23:15:19 +0100

    filter browser not complient hotkeys

--- src/hotkeys/hotkeyResolver.js
+++ src/hotkeys/hotkeyResolver.js
@@ -1,5 +1,18 @@
 export default function resolve(editor, keymapType) {
-  return require(`./${editor}/keymap.${keymapType
+  var hotkeys = require(`./${editor}/keymap.${keymapType
     .toLowerCase()
     .replace(" ", "")}.json`);
+
+  for (const cat in hotkeys) {
+    hotkeys[cat] = hotkeys[cat].filter(h => isHotkeyBrowserCompliant(h.value));
+  }
+
+  return hotkeys;
+}
+
+function isHotkeyBrowserCompliant(shortcuts) {
+  return shortcuts.every(s => {
+    const splitted = s.split("+").flatMap(x => x.split(" "));
+    return !(splitted.includes("w") && splitted.includes("ctrl"));
+  });
 }
