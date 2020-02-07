ec28a8f5cf9248e927f7717d931c2b9e1a28249e
undernotic <undernotic@gmail.com>
2020-02-07 17:36:24 +0100

    better look on smaller screens

--- src/main/index.js
+++ src/main/index.js
@@ -7,16 +7,18 @@ import katanaImg from "common/assets/katana/katana.png";
 
 export default function() {
   return (
-    <div className={`container ${styles["main-container"]}`}>
-      <div className="text-center">
-        <img className={styles.katana} src={katanaImg} alt="katana" />
-      </div>
-      <div className="row justify-content-center">
-        <div className="col-xl-9 col-lg-10">
-          <Router />
+    <>
+      <div className={`container ${styles["main-container"]}`}>
+        <div className="text-center">
+          <img className={styles.katana} src={katanaImg} alt="katana" />
+        </div>
+        <div className="row justify-content-center">
+          <div className="col-xl-9 col-lg-10">
+            <Router />
+          </div>
         </div>
       </div>
       <Footer />
-    </div>
+    </>
   );
 }
